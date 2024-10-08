ac6c560b56074289f22bebb5c9f349f14ef544cd
BCCK <saddamkhoso203@gmail.com>
2024-10-09 02:45:52 +0500

    learning let and bloc

--- main.js
+++ main.js
@@ -1,61 +1,74 @@
-console.log('Hello World');
-console.log('My name is Saddam Khoso');
-console.log('**');
-console.log('* *');
-console.log('*  *');
-console.log('*   *');
-console.log('*    *');
-console.log('*     *');
-console.log('*      *');
-console.log('*       *');
-console.log('*        *');
-console.log('*         *');
-console.log('*          *');
-console.log('*************');
+// console.log('Hello World');
+// console.log('My name is Saddam Khoso');
+// console.log('**');
+// console.log('* *');
+// console.log('*  *');
+// console.log('*   *');
+// console.log('*    *');
+// console.log('*     *');
+// console.log('*      *');
+// console.log('*       *');
+// console.log('*        *');
+// console.log('*         *');
+// console.log('*          *');
+// console.log('*************');
 
-console.log("2nd one");
-console.log("***********");
-console.log("**********");
-console.log("*********");
-console.log("********");
-console.log("*******");
-console.log("******");
-console.log("*****");
-console.log("****");
-console.log("***");
-console.log("**");
-console.log("*");
+// console.log("2nd one");
+// console.log("***********");
+// console.log("**********");
+// console.log("*********");
+// console.log("********");
+// console.log("*******");
+// console.log("******");
+// console.log("*****");
+// console.log("****");
+// console.log("***");
+// console.log("**");
+// console.log("*");
 
-console.log("Squre");
+// console.log("Squre");
 
-console.log("******");
-console.log("******");
-console.log("******");
-console.log("******");
+// console.log("******");
+// console.log("******");
+// console.log("******");
+// console.log("******");
 
-console.log("3rd one");
+// console.log("3rd one");
 
-console.log("*");
-console.log(" **");
-console.log("* **");
-console.log("****");
-console.log("*****");
-console.log("*****");
-console.log("*****");
-console.log("*****");
-console.log("****");
-console.log("***");
-console.log("**");
-console.log("*");
-console.log("4th one");
+// console.log("*");
+// console.log(" **");
+// console.log("* **");
+// console.log("****");
+// console.log("*****");
+// console.log("*****");
+// console.log("*****");
+// console.log("*****");
+// console.log("****");
+// console.log("***");
+// console.log("**");
+// console.log("*");
+// console.log("4th one");
 
 
 
-console.log(' *      *');
-console.log('  *    *');
-console.log('   *  *');
-console.log('    **');
-console.log('    *');
-console.log('   *  * ');
-console.log('  *    * ');
-console.log(' *      * ');
+// console.log(' *      *');
+// console.log('  *    *');
+// console.log('   *  *');
+// console.log('    **');
+// console.log('    *');
+// console.log('   *  * ');
+// console.log('  *    * ');
+// console.log(' *      * ');
+
+
+
+// console.log("practice");
+
+let username = "Saddam";
+username ='yasir';
+console.log(username);
+
+{
+    let username = "john";
+    console.log(username);
+}
