6606520b970adbac3d0326783e15da14a7b3d954
BCCK <saddamkhoso203@gmail.com>
2024-10-09 01:40:53 +0500

    assignment done

--- main.js
+++ main.js
@@ -1,2 +1,61 @@
 console.log('Hello World');
-console.log('My name is Saddam Khoso')
+console.log('My name is Saddam Khoso');
+console.log('**');
+console.log('* *');
+console.log('*  *');
+console.log('*   *');
+console.log('*    *');
+console.log('*     *');
+console.log('*      *');
+console.log('*       *');
+console.log('*        *');
+console.log('*         *');
+console.log('*          *');
+console.log('*************');
+
+console.log("2nd one");
+console.log("***********");
+console.log("**********");
+console.log("*********");
+console.log("********");
+console.log("*******");
+console.log("******");
+console.log("*****");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+
+console.log("Squre");
+
+console.log("******");
+console.log("******");
+console.log("******");
+console.log("******");
+
+console.log("3rd one");
+
+console.log("*");
+console.log(" **");
+console.log("* **");
+console.log("****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("*****");
+console.log("****");
+console.log("***");
+console.log("**");
+console.log("*");
+console.log("4th one");
+
+
+
+console.log(' *      *');
+console.log('  *    *');
+console.log('   *  *');
+console.log('    **');
+console.log('    *');
+console.log('   *  * ');
+console.log('  *    * ');
+console.log(' *      * ');
